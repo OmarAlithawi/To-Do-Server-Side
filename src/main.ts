80b0c8be79328085f26b3627be30537f3534a0bf
OmarAlithawi <oadnan100@gmail.com>
2020-10-15 11:52:16 +0300

    fixing start script

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import * as config from 'config';
 async function bootstrap() {
   const serverConfig = await config.get('server');
   const app = await NestFactory.create(AppModule);
-  if (process.env.NODE_ENV == 'development') {
-    app.enableCors();
-  } else {
-    app.enableCors();
-  }
+
+    if(process.env.NODE_ENV === 'development'){
+      app.enableCors();
+    }else{
+      app.enableCors();
+    }
+
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
 }
